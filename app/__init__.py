5012499c1a7b865726fdfb56580528b01ff4757c
Naomi Lai <nlai50@stuy.edu>
2024-12-06 10:58:07 -0500

    progress on logging in

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,10 @@
 # Imports
 from flask import Flask, request, render_template, redirect, url_for, flash, session
 import os
+from databases import login_user, init_db
 # from database import create_user, login_user, logout_user, create_story, create_edit, get_stories, can_add_to_story, add_to_story, get_contributors
+
+init_db()
 
 # Secret key/setup
 app = Flask(__name__)
@@ -25,6 +28,19 @@ def home():
 # Login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method=='POST':
+        # username = request.form.get('username')
+        # password = request.form.get('password')
+
+        # #  If fields not filled out
+        # if not username or not password:
+        #     redirect('/login', messaages="Please fill out both fields")
+
+        # Validate credentials (to be inserted from databases.py)
+        login_user()
+        return redirect("/")
+
+
     if 'username' in session:
         return redirect('/')
     return render_template('login.html')
--- app/databases.py
+++ app/databases.py
@@ -5,15 +5,15 @@
 # 2024-12-04
 # Time spent: ___
 
-from flask import request, flash, session
+from flask import Flask, request, render_template, redirect, url_for, flash, session
 import sqlite3
 import os
 
-app = Flask(__name__, template_folder='/templates', static_folder='/static')
+app = Flask(__name__)
 app.secret_key = os.urandom(32)
 
 def init_db():
-    """initialize db if none exists""""
+    """initialize db if none exists"""
     conn = sqlite3.connect('user_info.db')
     cursor = conn.cursor()
     # User table
@@ -24,36 +24,54 @@ def init_db():
             password TEXT NOT NULL
         )
     ''')
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS recipes (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            ingredients TEXT NOT NULL
-            content TEXT NOT NULL
-            name TEXT NOT NULL
-        )
-    ''')
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS food (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL
-            image BLOB
-            recipes REFERENCES recipes(name)
-        )
-    ''')
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS news (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            title TEXT UNIQUE NOT NULL
-            content TEXT NOT NULL
-        )
-    ''')
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS favorite_recipes (
-            user INTEGER PRIMARY KEY AUTOINCREMENT,
-            recipes REFERENCES recipes(id)
-        )
-    ''')
+    # cursor.execute('''
+    #     CREATE TABLE IF NOT EXISTS recipes (
+    #         id INTEGER PRIMARY KEY AUTOINCREMENT,
+    #         ingredients TEXT NOT NULL
+    #         content TEXT NOT NULL
+    #         name TEXT NOT NULL
+    #     )
+    # ''')
+    # cursor.execute('''
+    #     CREATE TABLE IF NOT EXISTS food (
+    #         id INTEGER PRIMARY KEY AUTOINCREMENT,
+    #         name TEXT NOT NULL
+    #         image BLOB
+    #         recipes REFERENCES recipes(name)
+    #     )
+    # ''')
+    # cursor.execute('''
+    #     CREATE TABLE IF NOT EXISTS news (
+    #         id INTEGER PRIMARY KEY AUTOINCREMENT,
+    #         title TEXT UNIQUE NOT NULL
+    #         content TEXT NOT NULL
+    #     )
+    # ''')
+    # cursor.execute('''
+    #     CREATE TABLE IF NOT EXISTS favorite_recipes (
+    #         user INTEGER PRIMARY KEY AUTOINCREMENT,
+    #         recipes REFERENCES recipes(id)
+    #     )
+    # ''')
     conn.commit()
     conn.close()
 
-init_db()
+# init_db()
+
+def login_user():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    # check if fields filled out
+    if not username or not password:
+        print("fill out")
+        redirect('/login')
+
+    with sqlite3.connect('user_info.db'):
+        cursor = conn.connect()
+        cursor.execute('SELECT password FROM users WHERE username=?', (username,))
+        result = cursor.fetchone()
+
+        if result:
+            print(result)
+        conn.close()
