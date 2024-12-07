c2f7278ef4f284c65022d640a4685879217be606
esokul007 <esokul007@gmail.com>
2024-12-06 18:32:28 -0800

    Base functions to get info for brewery, article, and recipes API

--- app/API.py
+++ app/API.py
@@ -4,12 +4,34 @@ from urllib.request import Request
 from urllib.request import urlopen
 import json
 import random
+import requests
 
 
 def getBrews(): # Doesn't work now because forbidden from url???
     url = "https://api.openbrewerydb.org/v1/breweries"
+    headers={"User-Agent" : "Mozilla/5.0"} # Authorizes request. Tells API that you're not a bot???
+    request = urllib.request.Request(url, headers=headers)
+    API = urllib.request.urlopen(request)
+    data = json.loads(API.read()) # List of dictionaries. 50 brewery locations.
+    print(data)
+
+def getArticles():
+    file = open('keys/NYT_API_key.txt', 'r')
+    content = file.read()
+    file.close()
+    url = f"https://api.nytimes.com/svc/search/v2/articlesearch.json?q=food&q=dining&api-key={content}" # Food and DIning Articles as query
     API = urllib.request.urlopen(url)
-    python_ify = json.loads(API.read())
-    #return [python_ify['id'], python_ify['advice']]
-    return(python_ify)
-getLoc()
+    data = json.loads(API.read())
+    articles = data['response']['docs'] 
+    urls = [article['web_url'] for article in articles] # url's of articles in past month
+    print(urls)
+
+def getRecipes():
+    file = open('keys/Spoonacular_API_key.txt', 'r')
+    content = file.read().strip()
+    file.close()
+    url = f"https://api.spoonacular.com/recipes/random?apiKey={content}"
+    response = requests.get(url)# Get Request of URL. API Key is required at end.
+    data = response.json()# random recipe and all its info
+    print(data)
+
