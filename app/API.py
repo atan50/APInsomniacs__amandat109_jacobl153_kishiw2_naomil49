62c6e125ae3058922a4a1a573487a738fbec3dd8
esokul007 <esokul007@gmail.com>
2024-12-09 11:02:48 -0500

    Pulling important info from APIs

--- app/API.py
+++ app/API.py
@@ -4,7 +4,7 @@ from urllib.request import Request
 from urllib.request import urlopen
 import json
 import random
-# import requests
+import requests
 
 
 def getBrews(): # Doesn't work now because forbidden from url???
@@ -14,24 +14,29 @@ def getBrews(): # Doesn't work now because forbidden from url???
         request = urllib.request.Request(url, headers=headers)
         API = urllib.request.urlopen(request)
         data = json.loads(API.read()) # List of dictionaries. 50 brewery locations.
-        print(data)
+        names = [article['name'] for article in data]
+        longitude = [article['longitude'] for article in data]
+        latitude = [article['latitude'] for article in data]
+        print(names)
     except Exception as e:
-        return e
+        print(e)
 
 
 def getArticles():
     try:
         file = open('keys/NYT_API_key.txt', 'r')
-        content = file.read()
+        content = file.read().strip()
         file.close()
         url = f"https://api.nytimes.com/svc/search/v2/articlesearch.json?q=food&q=dining&api-key={content}" # Food and DIning Articles as query
         API = urllib.request.urlopen(url)
         data = json.loads(API.read())
         articles = data['response']['docs']
         urls = [article['web_url'] for article in articles] # url's of articles in past month
-        print(urls)
+        lead_par = [article['lead_paragraph'] for article in articles]
+        headline = [article['headline']['print_headline'] for article in articles]
+        print(headline)
     except Exception as e:
-        return e
+        print(e)
 
 def getRecipes():
     try:
@@ -41,6 +46,8 @@ def getRecipes():
         url = f"https://api.spoonacular.com/recipes/random?apiKey={content}"
         response = requests.get(url)# Get Request of URL. API Key is required at end.
         data = response.json()# random recipe and all its info
-        print(data)
+        ingredients = data['recipes'][0]['extendedIngredients']
+        print(ingredients)
     except Exception as e:
-        return e
+        print(e) 
+getBrews()
